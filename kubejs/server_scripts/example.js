fe5616c4ec93f4aa33d4fec41b8f34d11e5a6b44
WhiteTraveller <Zhaomargatroid@outlook.com>
2026-03-23 00:45:12 +0800

    [feature] 测试搓脚ui界面开发

--- kubejs/server_scripts/example.js
+++ kubejs/server_scripts/example.js
@@ -9,9 +9,7 @@ ItemEvents.rightClicked(event => {
 global.rightClickedEntity = event => {
     let target = event.getTarget().entity
     console.log(target)
-    event.player.sendData('open_foot_ui', {})
-    ApricityUI
-    
+    ApricityUI.openScreen(event.player, 'kubejs/footui.html', null)
     // 检查是否是寻路系统创建的实体（通过 pfPhase 判断）
     if (target && target.persistentData) {
         let pfPhase = target.persistentData.getInt("pfPhase")
@@ -21,4 +19,5 @@ global.rightClickedEntity = event => {
             event.player.sendData('open_foot_ui', {})
         }
     }
+    // event.player.sendData('open_foot_ui', {})
 }
